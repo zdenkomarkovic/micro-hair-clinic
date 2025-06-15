365c34fee51e0fba629115e3f856ec99b64b9da9
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-06-15 17:55:46 +0200

    .

--- components/TableSection.tsx
+++ components/TableSection.tsx
@@ -1,4 +1,3 @@
-// components/TableSection.tsx
 type Props = {
   data: {
     id: string;
@@ -8,12 +7,23 @@ type Props = {
 };
 
 export default function TableSection({ data }: Props) {
+  const [headerRow, ...bodyRows] = data.table;
+
   return (
     <section id={data.id} className="py-6">
       <h2 className="text-2xl mb-4">{data.title}</h2>
-      <table className="w-full border">
+      <table className="w-full border border-gray-300">
+        <thead>
+          <tr>
+            {headerRow.map((cell, j) => (
+              <th key={j} className="p-2 border bg-gray-100 text-left">
+                {cell}
+              </th>
+            ))}
+          </tr>
+        </thead>
         <tbody>
-          {data.table.map((row, i) => (
+          {bodyRows.map((row, i) => (
             <tr key={i} className="border-t">
               {row.map((cell, j) => (
                 <td key={j} className="p-2 border">
