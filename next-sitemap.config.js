b4c86ae0bff6337620929e5a68c2bb2a7f3f6c45
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-07-08 11:14:34 +0200

    .

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://www.microhairclinic.si",
+  siteUrl: "https://microhairclinic.si",
   generateRobotsTxt: true,
   sitemapSize: 5000,
   i18n: {
-    locales: ["sl", "en", "de"],
+    locales: ["sl", "en", "de"], // Dodaj sve jezike koje koristiš
     defaultLocale: "sl",
   },
 };
