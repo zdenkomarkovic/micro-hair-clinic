4fa920057e815cafd05198150a1ff145b6ee778d
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-07-08 02:24:36 +0200

    .

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,18 +1,10 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://microhairclinic.si",
+  siteUrl: "https://www.microhairclinic.si",
   generateRobotsTxt: true,
   sitemapSize: 5000,
-  outDir: "public",
-  transform: async (config, path) => {
-    // Izbaci "_not-found" rute itd ako ih imaš
-    if (path.includes("/_")) return null;
-
-    return {
-      loc: path,
-      changefreq: "weekly",
-      priority: 0.7,
-      lastmod: new Date().toISOString(),
-    };
+  i18n: {
+    locales: ["sl", "en", "de"],
+    defaultLocale: "sl",
   },
 };
